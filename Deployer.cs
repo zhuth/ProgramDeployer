d86dc78cf6a59717b22fdac6adf2d81823bd64f0
ZTH <zthpublic@gmail.com>
2014-02-11 10:07:53 +0800

    deployer signature

--- Deployer.cs
+++ Deployer.cs
@@ -39,6 +39,8 @@ namespace ProgramDeployerServer
 
         public void Run(string line)
         {
+            ProgramDeployerClient.PropertiesParser props = new ProgramDeployerClient.PropertiesParser("");
+
             line = noannoations(line).Trim();
             string[] args = split(line);
 
@@ -85,10 +87,15 @@ namespace ProgramDeployerServer
                         try
                         {
                             Console.WriteLine("- proc:" + args[1] + " " + args[2] + " @" + client);
-                            new WebClient().DownloadString(client + "proc?action=" + args[1] + "&name=" + args[2]);
+                            Console.WriteLine(ProgramDeployerClient.Httpd.DownloadString(client + "/proc?action=" + args[1] + "&name=" + args[2], Properties.Settings.Default.HashKey));
                         }
                         catch { }
                     }
+                    break;
+                case "download":
+                    if (args.Length < 2) return;
+                    string path = ProgramDeployerClient.Httpd.GetSignedUrl(args[1], Properties.Settings.Default.HashKey);
+                    System.Diagnostics.Process.Start(path);
                     break;
             }
         }
@@ -98,14 +105,14 @@ namespace ProgramDeployerServer
             if (!File.Exists(localFilePath)) return;
 
             Console.WriteLine("- " + localFilePath);
-            string localMD5 = ProgramDeployerClient.FileMD5.HashFile(localFilePath, "md5");
+            string localMD5 = ProgramDeployerClient.MD5Crypt.HashFile(localFilePath, "md5");
             foreach (string client in _clients)
             {
                 int trials = 0;
                 bool doPush = false;
                 try
                 {
-                    string md5 = new WebClient().DownloadString(client + remotePath + "?md5").Trim();
+                    string md5 = ProgramDeployerClient.Httpd.DownloadString(client + remotePath + "?md5", Properties.Settings.Default.HashKey).Trim();
                     if (localMD5 != md5) doPush = true;
                 }
                 catch (WebException)
@@ -117,7 +124,7 @@ namespace ProgramDeployerServer
             Retry:
                 try
                 {
-                    ProgramDeployerClient.Httpd.PushFile(client + remotePath, localFilePath);
+                    ProgramDeployerClient.Httpd.PushFile(client + remotePath, localFilePath, Properties.Settings.Default.HashKey);
                 }
                 catch(Exception ex)
                 {
